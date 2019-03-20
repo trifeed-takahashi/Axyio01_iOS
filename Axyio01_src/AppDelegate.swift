38ebcc31d8b37f68098e73890851d0e867744ef8
高橋聖二 <m.takahashi@trifeed.jp>
2019-03-20 12:18:25 +0900

    Swift4.2対応

--- Axyio01_src/AppDelegate.swift
+++ Axyio01_src/AppDelegate.swift
@@ -23,7 +23,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
     var viewController: ViewController!
     var alert_id = ""
     
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
         // Override point for customization after application launch.
         let center = UNUserNotificationCenter.current()
